21cab5c24eac8d2bbcde9a874ad9657098df9973
devaeterne <info@safaksariyildiz.com>
2024-02-01 12:21:49 +0300

    time and send list

--- main.py
+++ main.py
@@ -11,19 +11,22 @@ def send_whatsapp_message(msg: str, phone_numbers: list):
             pywhatkit.sendwhatmsg_instantly(
                 phone_no=phone_no,
                 message=msg,
-                tab_close=False
+                ##close whatsappTabs
+                tab_close=True
             )
-            ##time.sleep(10)
-            pyautogui.click()
-            time.sleep(2)
-            keyboard.press(Key.enter)
+            ##time.sleep(1)
+            ##pyautogui.click()
+            time.sleep(1)
+            ##keyboard.press(Key.enter)
             keyboard.release(Key.enter)
+            keyboard.press(Key.esc)
             print(f"Message sent to {phone_no}!")
     except Exception as e:
         print(str(e))
 if __name__ == "__main__":
     # List of phone numbers of your customers
     with open('list.py') as f:
-        lines = [line.rstrip('\n') for line in f]
+        lines = [line.rstrip('\n') for line in f]        
     message = "Whatsapp Toplu mesaj Listesi..."
     send_whatsapp_message(msg=message, phone_numbers=lines)
+
